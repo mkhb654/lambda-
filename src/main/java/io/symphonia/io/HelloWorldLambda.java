9ceb7fffe04018888a7565ee8c01b95c6ece94b0
zen <zen@zens-MBP.fios-router.home>
2017-09-22 18:01:09 -0400

    working with lambda function on aws

---
+++ src/main/java/io/symphonia/io/HelloWorldLambda.java
@@ -0,0 +1,16 @@
+package io.symphonia.io;
+
+/** Working with Lambda serverless applicatoin.
+ * Created by zen on 9/22/17.
+ */
+public class HelloWorldLambda {
+
+    public void String handler(String input){
+        String message = String.format("hello", "%s!", input);
+        System.out.println(message);
+        return message;
+
+    }
+
+
+}
